31d13e4ab75998ed4e5409859e400156e8e1765f
hugoadigio <smallville99.45@gmail.com>
2013-11-13 05:08:23 +0000

    canvas-caritas

--- htdocs/js/canvas.js
+++ htdocs/js/canvas.js
@@ -1,58 +1,116 @@
 //realizar acciones al cargar al documento
 window.onload = function() {
-  //asociar un evento al boton
-  var boton = document.getElementById('ver');
-  boton.onclick=mostrar;
+    //asociar un evento al boton
+    var boton = document.getElementById('ver');
+    boton.onclick=mostrar;
 }
 
 function mostrar() {
-   var canvas = document.getElementById('drawzone');
-   var context=canvas.getContext('2d');
-   limpiar(canvas,context);
-  //leer opciones que el usuario da
-   var figura = document.getElementById('figura');
-   var indice=figura.selectedIndex;
-   var seleccionFigura= figura[indice].value;
-   
-  if (seleccionFigura=='cuadrados') {
-    //dibujar 20 cuadrados
-    for (var i=0;i<20;i++) {
-      //llamar a la funcion dibujarCuadrado
-       dibujarCuadrado(canvas,context);
-    }
-  } else if (seleccionFigura=='circulos' ) {
+    var canvas = document.getElementById('drawzone');
+    var context=canvas.getContext('2d');
+    
+    limpiar(canvas,context);
+    //leer opciones que el usuario da
+    var figura = document.getElementById('figura');
+    var indice=figura.selectedIndex;
+    var seleccionFigura= figura[indice].value;
+    
+    if (seleccionFigura=='cuadrados') {
+        //dibujar 20 cuadrados
+        for (var i=0;i<20;i++) {
+            //llamar a la funcion dibujarCuadrado
+            dibujarCuadrado(canvas,context);
+        }
+    } else if (seleccionFigura=='circulos' ) {
         //dibujar 20 circulos
-    for (var i=0;i<20;i++) {
-      //llamar a la funcion dibujarCuadrado
-       dibujarCirculo(canvas,context);
+        for (var i=0;i<20;i++) {
+            //llamar a la funcion dibujarCuadrado
+            dibujarCirculo(canvas,context);
+        }
+    } else if (seleccionFigura=='semicirculos' ) {
+        //dibujar 20 circulos
+        for (var i=0;i<20;i++) {
+            //llamar a la funcion dibujarCuadrado
+            dibujarSemiCirculo(canvas,context);
+        }
+    }else if (seleccionFigura=='caritas' ) {
+        //dibujar 20 caritas
+        for (var i=0;i<20;i++) {
+            //llamar a la funcion dibujarCaritas
+            dibujaCaritas(canvas,context);
+        }
     }
-  }
 }
 
 function dibujarCuadrado(canvas,contexto) {
-  //generar de manera aleatoria las propiedades del cuadrado
-  var ancho = Math.floor(Math.random()*40 );
-  var x = Math.floor(Math.random()*canvas.width);
-  var y = Math.floor(Math.random()*canvas.height);
-  contexto.fillStyle = 'lightblue';
-  contexto.fillRect(x,y,ancho,ancho);
+    //generar de manera aleatoria las propiedades del cuadrado
+    var ancho = Math.floor(Math.random()*40 );
+    var x = Math.floor(Math.random()*canvas.width);
+    var y = Math.floor(Math.random()*canvas.height);
+    contexto.fillStyle = 'lightblue';
+    contexto.fillRect(x,y,ancho,ancho);
 }
 
 function dibujarCirculo(canvas,contexto) {
-  var radio = Math.floor(Math.random()*20);
-  var x = Math.floor(Math.random()*canvas.width);
-  var y = Math.floor(Math.random()*canvas.height);
-  
-  contexto.beginPath();
-  contexto.arc(x,y,radio,0, 2*Math.PI,true)
-  contexto.fillStyle='lightblue';
-  contexto.fill();
+    var radio = Math.floor(Math.random()*40);
+    var x = Math.floor(Math.random()*canvas.width);
+    var y = Math.floor(Math.random()*canvas.height);
+    
+    contexto.beginPath();
+    contexto.arc(x,y,radio,0, 2*Math.PI,true)
+    contexto.fillStyle='lightblue';
+    contexto.fill();
+}
+
+function dibujaCaritas(canvas,contexto) {
+    
+    var radio = Math.floor(Math.random()*40);
+    var x = Math.floor(Math.random()*canvas.width);
+    var y = Math.floor(Math.random()*canvas.height);
+   
+		//CARA
+		contexto.beginPath();
+		contexto.arc(x, y, radio, 0, 2*Math.PI,true)
+		contexto.fillStyle = "#FF9999";
+		contexto.fill();
+	   
+		
+		//OJO IZQQUIERDO
+		contexto.beginPath();
+		contexto.arc(x+(radio/2),y-(radio/2),radio/5, 0, 2*Math.PI, true);
+		contexto.fillStyle = "#99FF99";
+		contexto.fill();
+		
+		
+		//OJO DERECHO
+		 contexto.beginPath();
+		 contexto.arc(x-(radio/2),y-(radio/2),radio/5, 0, 2*Math.PI, true);
+		 contexto.fillStyle = "#99FF99";
+		 contexto.fill();
+	  	
+		
+		//BOCA
+		contexto.beginPath();
+		contexto.arc(x,y,radio/2,0, 5*Math.PI/5,false);
+		contexto.fillStyle="#FF0066";
+		contexto.fill();
+}
+
+function dibujarSemiCirculo(canvas,contexto) {
+    var radio = Math.floor(Math.random()*40);
+    var x = Math.floor(Math.random()*canvas.width);
+    var y = Math.floor(Math.random()*canvas.height);
+    
+    contexto.beginPath();
+    contexto.arc(x,y,radio,Math.PI/4, 5*Math.PI/4,true)
+    contexto.fillStyle='lightblue';
+    contexto.fill();
 }
 
 function limpiar(canvas,contexto) {
-  var colorFondo = document.getElementById('colorFondo');
-  var indice = colorFondo.selectedIndex;
-  var seleccionado = colorFondo.options[indice].value;
-  contexto.fillStyle = seleccionado;
-  contexto.fillRect(0,0,canvas.width, canvas.height);
+    var colorFondo = document.getElementById('colorFondo');
+    var indice = colorFondo.selectedIndex;
+    var seleccionado = colorFondo.options[indice].value;
+    contexto.fillStyle = seleccionado;
+    contexto.fillRect(0,0,canvas.width, canvas.height);
 }
